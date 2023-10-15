2abd604b58a5e6a458ea544c345bb1ac705d2199
ingSebastian <jssierrapi@unadvirtual.edu.co>
2023-10-15 08:49:49 -0500

    ListaProduct-insert js

--- main.js
+++ main.js
@@ -1,9 +1,99 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
+menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toogleMobileMenu);
+menuCarritoIcon.addEventListener("click", toogleCarritoAside);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
-   
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+  desktopMenu.classList.toggle("inactive");
 }
+
+function toogleMobileMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toogleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+
+  aside.classList.toggle("inactive");
+}
+
+const productList = [];
+productList.push({
+  name: "Biker Jacket",
+  price: 230,
+  img: "https://images.pexels.com/photos/5325899/pexels-photo-5325899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+});
+productList.push({
+  name: "YEANS",
+  price: 230,
+  img: "https://images.pexels.com/photos/45982/pexels-photo-45982.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+});
+
+productList.push({
+  name: "Blusa con botones",
+  price: 230,
+  img: "https://images.pexels.com/photos/1502216/pexels-photo-1502216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+});
+
+function addProduct(insert) {
+  for (product of insert) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+  
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.img);
+  
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+  
+    const productInfoDiv = document.createElement("div");
+  
+    const productPrice = document.createElement("p");
+    productPrice.innerHTML = "$" + product.price;
+    const productName = document.createElement("p");
+    productName.innerHTML = product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+    const productInfoFigure = document.createElement("figure");
+    const productImgCart = document.createElement("img");
+  
+    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
+  
+
+}
+
+addProduct(productList);
